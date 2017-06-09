8055a4634e792a1c76476ceff679691e6843cf94
Nicolas Noury <noury@archos.com>
2017-06-09 15:29:51 +0200

    import legacy libav configure parameters

---
+++ config_base.sh
@@ -0,0 +1,83 @@
+CONFIG_LIBAV="\
+	--disable-programs \
+	--disable-swresample \
+	--disable-swscale \
+	--disable-avfilter \
+	--disable-avresample \
+	--disable-bzlib \
+	--disable-muxers \
+	--disable-demuxers \
+	--disable-bsfs \
+	--disable-protocols \
+	--disable-avdevice \
+	--disable-devices \
+	--disable-filters \
+	--disable-encoders \
+	--disable-decoders \
+	\
+	--enable-decoder=cook \
+	--enable-decoder=flac \
+	--enable-decoder=aac \
+	--enable-decoder=aac_latm \
+	--enable-decoder=mp2 \
+	--enable-decoder=mp3 \
+	--enable-decoder=wavpack \
+        --enable-decoder=tta \
+	--enable-decoder=vorbis \
+	--enable-decoder=ac3 \
+	\
+	--enable-decoder=amrnb \
+	--enable-decoder=amrwb \
+	\
+	--enable-decoder=msmpeg4v1 \
+	--enable-decoder=msmpeg4v2 \
+	--enable-decoder=msmpeg4v3 \
+	--enable-decoder=h263 \
+	--enable-decoder=h264 \
+	--enable-decoder=hevc \
+	--enable-decoder=mpeg4 \
+	--enable-decoder=mpegvideo \
+	--enable-decoder=mpeg1video \
+	--enable-decoder=flv \
+	--enable-decoder=rv10 \
+	--enable-decoder=rv20 \
+	--enable-decoder=rv30 \
+	--enable-decoder=rv40 \
+	--enable-decoder=mjpeg \
+	--enable-decoder=vp6f \
+	--enable-decoder=vp8 \
+	--enable-decoder=wmv1 \
+	--enable-decoder=wmv2 \
+	--enable-decoder=wmv3 \
+	--enable-decoder=vc1 \
+	--enable-decoder=mpeg2video \
+	--enable-decoder=theora \
+	\
+	--enable-demuxer=avi \
+	--enable-demuxer=matroska \
+	--enable-demuxer=rtsp \
+	--enable-demuxer=sdp \
+	--enable-demuxer=aac \
+	--enable-demuxer=mp3 \
+	--enable-demuxer=h261 \
+	--enable-demuxer=h263 \
+	--enable-demuxer=h264 \
+	--enable-demuxer=hevc \
+	--enable-demuxer=mpegts \
+	--enable-demuxer=mpegtsraw \
+	--enable-demuxer=mpegps \
+	--enable-demuxer=mpegvideo \
+	--enable-demuxer=wtv \
+	--enable-demuxer=ac3 \
+	--enable-demuxer=ogg \
+	--enable-demuxer=flv \
+	--enable-demuxer=dnxhd \
+	\
+	--enable-protocol=file \
+	--enable-protocol=http \
+	--enable-protocol=rtp \
+	--enable-protocol=tcp \
+	--enable-protocol=udp \
+	\
+	--enable-parsers \
+	--enable-muxer=spdif"
---
+++ config_mpeg2.sh
@@ -0,0 +1,4 @@
+. ./config_base.sh
+CONFIG_LIBAV="${CONFIG_LIBAV} \
+	--enable-decoder=mpeg2video \
+	--enable-decoder=mp2"
