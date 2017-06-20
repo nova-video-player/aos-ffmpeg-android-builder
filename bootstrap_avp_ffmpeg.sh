7b5e0679d7467b47f09e1e0c774a491e16a55cf4
Nicolas Noury <noury@archos.com>
2017-06-20 14:12:27 +0200

    add a bootstrap script

---
+++ bootstrap_avp_ffmpeg.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+for ARCH in arm x86; do
+	if [ ${ARCH} == arm ] ; then
+		ABI=armeabi-v7a
+	else
+		ABI=x86
+	fi
+	if [[ ! -d dist-base-${ABI} ]]; then
+		( . build.sh -a ${ARCH} -c config_base.sh )
+	fi
+done
