00d3cad1713bd48f5c412367582eaeaf6e8d3562
Courville Software <software@courville.org>
2020-07-31 19:19:38 +0200

    Revert "new 4.3.0 binaries after dav1d master update"
    This reverts commit a805550c49c5127459109686af8ed775c8aaf3b0.
    
    On arm64 causes video to stop often.

--- bootstrap_avp_ffmpeg.sh
+++ bootstrap_avp_ffmpeg.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-if [ `meson --version` == "0.53.2" ] 
-then
-  echo meson version compatible with x86 build
-else
-  echo ERROR: meson version not compatible with x86 build, pip3 install meson==0.53.2
-  exit 1
-fi
 for ARCH in arm arm64 x86 x86_64; do
 	case "${ARCH}" in
     'arm')
