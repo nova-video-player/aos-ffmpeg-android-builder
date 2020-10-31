c6409f96a131a42c0e18850184f5147dcab883be
Courville Software <software@courville.org>
2020-10-31 16:36:34 +0100

    ident

--- bootstrap_avp_ffmpeg.sh
+++ bootstrap_avp_ffmpeg.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-for ARCH in arm arm64 x86 x86_64; do
-	case "${ARCH}" in
+for ARCH in arm arm64 x86 x86_64
+do
+  case "${ARCH}" in
     'arm')
-		ABI=armeabi-v7a ;;
+      ABI=armeabi-v7a ;;
     'arm64')
-        ABI=arm64-v8a ;;
+      ABI=arm64-v8a ;;
     *)
-        ABI=${ARCH} ;;
+      ABI=${ARCH} ;;
     esac
-	if [[ ! -d dist-full-${ABI} ]]; then
-		( . build.sh -a ${ARCH} -c config_full.sh )
-	fi
-
+  if [ ! -d dist-full-${ABI} ]
+  then
+   ./build.sh -a ${ARCH} -c config_full.sh
+  fi
 done
